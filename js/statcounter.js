4e77eeae0934362c9f32e779a54bf652ba54e3e2
suprunidze <suprun.serhiy@gmail.com>
2025-09-11 20:46:40 +0300

    Update statcounter.js

--- js/statcounter.js
+++ js/statcounter.js
@@ -1,32 +1,31 @@
-        const apiEndpoint = 'http://e9yh7t2uanm.tplinkdns.com:8080/api/page-views';
-        const pageLoadTime = new Date();
-
-        const sendPageView = (timeSpent) => {
-            fetch(apiEndpoint, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    page: window.location.href,
-                    time_spent: timeSpent
-                })
-            })
-            .then(response => {
-                if (!response.ok) {
-                    console.error('Failed to send page view data.');
-                }
-            })
-            .catch(error => {
-                console.error('Error sending page view data:', error);
-            });
-        };
-
-        document.addEventListener('DOMContentLoaded', () => {
-            sendPageView(0);
-        });
-
-        setInterval(() => {
-            const timeSpent = (new Date() - pageLoadTime) / 1000;
-            sendPageView(timeSpent);
-        }, 15000);
+const apiEndpoint = 'http://e9yh7t2uanm.tplinkdns.com:8080/api/page-views';
+const pageLoadTime = new Date();
+const sendPageView = (timeSpent) => {
+    fetch(apiEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            page: window.location.href,
+            time_spent: timeSpent
+        })
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.error('Failed to send page view data.');
+        }
+    })
+    .catch(error => {
+        console.error('Error sending page view data:', error);
+    });
+};
+// Надсилання першого запиту без часу
+document.addEventListener('DOMContentLoaded', () => {
+    sendPageView(0);
+});
+// Надсилання оновлень кожні 30 секунд
+setInterval(() => {
+    const timeSpent = (new Date() - pageLoadTime) / 1000;
+    sendPageView(timeSpent);
+}, 15000); // 15 секунд у мілісекундах
